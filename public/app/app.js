6bb6b15e7c1f9334ab2cf8014d0efeb180ee59ff
kajlund <kaj.lund@gmail.com>
2014-03-17 21:46:05 +0200

    Added directive a-disabled for enabling/disabling click handlers on link elements

--- public/app/app.js
+++ public/app/app.js
@@ -43,4 +43,22 @@
             console.log('App Loaded');
         }]);
 
+    app.directive('aDisabled', function ($compile) {
+        return {
+            restrict: 'A',
+            priority: -99999,
+            link: function (scope, element, attrs) {
+                scope.$watch(attrs.aDisabled, function (val, oldval) {
+                    if (!!val) {
+                        element.unbind('click');
+                    } else if (oldval) {
+                        element.bind('click', function () {
+                            scope.$apply(attrs.ngClick);
+                        });
+                    }
+                });
+            }
+        };
+    });
+
 }(angular));
