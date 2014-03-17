38f60fc9ba3e858a954cf5a872f3d07e5d0a52a8
kajlund <kaj.lund@gmail.com>
2014-03-17 20:37:02 +0200

    Added SessionService with check for loggedon + methods for signon and signoff click

--- public/app/ctrlMain.js
+++ public/app/ctrlMain.js
@@ -1,15 +1,37 @@
 (function (angular) {
     'use strict';
 
-    angular.module('app').controller('MainController', ['$scope', '$rootScope', '$location', '$log',
-        function ($scope, $rootScope, $location, $log) {
+    angular.module('app').controller('MainController', ['$scope', '$rootScope', '$location', '$log', 'SessionService',
+        function ($scope, $rootScope, $location, $log, SessionService) {
 
-            $scope.getClass = function(path) {
-                if ($location.path().substr(0, path.length) == path) {
-                    return "active"
-                } else {
-                    return ""
-                }
+            $scope.session = SessionService;
+            $log.info(SessionService);
+
+            if (!SessionService.loggedOn()) {
+                $log.info('not logged on');
+                SessionService.autoSignon()
+                    .then(function (data) {
+                        $log.info($scope.session);
+                    }, function (err) {
+                        $log.error(err);
+                    });
             }
+
+            $scope.getClass = function (path) {
+                var className = "";
+
+                if ($location.path().substr(0, path.length) === path) {
+                    className = "active";
+                }
+                return className;
+            };
+
+            $scope.onSignonClick = function () {
+                $log.info('Signing on');
+            };
+
+            $scope.onSignoffClick = function () {
+                $log.info('Signing off');
+            };
         }]);
 }(angular));
