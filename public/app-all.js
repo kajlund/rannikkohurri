a2475f85c7a904da5506dca2a30b807060202fa9
kajlund <kaj.lund@gmail.com>
2014-03-18 13:08:47 +0200

    New build after adding login dialog

--- public/app-all.js
+++ public/app-all.js
@@ -1,13 +1,23 @@
-(function (angular) {
+(function (angular, toastr) {
     'use strict';
+
+    // Configure Toastr
+    toastr.options.timeOut = 2000;
+    toastr.options.positionClass = 'toast-bottom-right';
 
     var app = angular.module('app', [
         // Angular modules
-        'ngRoute'   // routing
+        'ngRoute',     // routing
+        'ngCookies',   // cookies
+        'ui.bootstrap' // ui-bootstrap library
     ]);
 
     // Configure Routes
-    app.config(['$routeProvider', function ($routeProvider) {
+    app.config(['$routeProvider', '$httpProvider',
+        function ($routeProvider, $httpProvider) {
+        $httpProvider.defaults.headers.common['X-Parse-Application-Id'] = 'HZAMesseJ6CDe1K5dFLfxbGbMYD6aV3lBaEp3Ib1';
+        $httpProvider.defaults.headers.common['X-Parse-REST-API-Key'] = 'LZqwu8VIutbaphzVoPW7yf4RxkKQAMbAapwubT5L';
+
         $routeProvider
             .when('/', {
                 redirectTo: '/home'
@@ -38,7 +48,25 @@
             console.log('App Loaded');
         }]);
 
-}(angular));
+    app.directive('aDisabled', function ($compile) {
+        return {
+            restrict: 'A',
+            priority: -99999,
+            link: function (scope, element, attrs) {
+                scope.$watch(attrs.aDisabled, function (val, oldval) {
+                    if (!!val) {
+                        element.unbind('click');
+                    } else if (oldval) {
+                        element.bind('click', function () {
+                            scope.$apply(attrs.ngClick);
+                        });
+                    }
+                });
+            }
+        };
+    });
+
+}(angular, toastr));
 (function (angular) {
     'use strict';
 
@@ -47,19 +75,163 @@
 
         }]);
 }(angular));
+(function (angular, toastr) {
+    'use strict';
+
+    angular.module('app').controller('MainController', ['$scope', '$rootScope', '$location', '$log', '$modal', 'SessionService',
+        function ($scope, $rootScope, $location, $log, $modal, SessionService) {
+
+            $scope.session = SessionService;
+            $log.info(SessionService);
+
+            if (!SessionService.loggedOn()) {
+                $log.info('not logged on');
+                SessionService.autoSignon()
+                    .then(function (data) {
+                        $log.info($scope.session);
+                    }, function (err) {
+                        $log.error(err);
+                    });
+            }
+
+            $scope.getClass = function (path) {
+                var className = "";
+
+                if ($location.path().substr(0, path.length) === path) {
+                    className = "active";
+                }
+                return className;
+            };
+
+            $scope.onSignonClick = function () {
+                var user = { name: '', pwd: '' },
+                    modalInstance = $modal.open({
+                        templateUrl: 'app/signon.html',
+                        controller: 'SignonController',
+                        resolve: {
+                            user: function () {
+                                return user;
+                            }
+                        }
+                    });
+
+                modalInstance.result.then(function () {
+                    SessionService.signon(user.name, user.pwd)
+                        .then(function (result) {
+                            toastr.info(SessionService.userObj.username + ' signed on');
+                        }, function (error) {
+                            toastr.error(error.error);
+                        });
+                }, function () {
+                    toastr.info('Signon cancelled');
+                });
+            };
+
+            $scope.onSignoffClick = function () {
+                $log.info('Signing off');
+                SessionService.signoff();
+                toastr.warning('User signed off');
+            };
+        }]);
+}(angular, toastr));
 (function (angular) {
     'use strict';
 
-    angular.module('app').controller('MainController', ['$scope', '$rootScope', '$location', '$log',
-        function ($scope, $rootScope, $location, $log) {
+    angular.module('app').controller('SignonController', ['$scope', '$modalInstance', 'user',
+        function ($scope, $modalInstance, user) {
+            $scope.user = user;
 
-            $scope.getClass = function(path) {
-                if ($location.path().substr(0, path.length) == path) {
-                    return "active"
-                } else {
-                    return ""
-                }
+            $scope.ok = function () {
+                $modalInstance.close('OK');
+            };
+
+            $scope.cancel = function () {
+                $modalInstance.dismiss('Cancel');
+            };
+        }]);
+}(angular));
+(function (angular) {
+    'use strict';
+
+    angular.module('app').factory('SessionService', ['$rootScope', '$http', '$cookieStore', '$log', '$q',
+        function ($rootScope, $http, $cookieStore, $log, $q) {
+            var res = {};
+            res.sessionToken = '';
+            res.userObj = null;
+
+            function setSession(data) {
+                res.userObj = data;
+                res.sessionToken = data.sessionToken;
+                $cookieStore.put('ParseUser', data);
+
             }
+            function clearSession() {
+                res.userObj = null;
+                res.sessionToken = '';
+                $cookieStore.remove('ParseUser');
+            }
+
+            res.autoSignon = function () {
+                var user = $cookieStore.get('ParseUser'),
+                    config = {
+                        headers: {
+                            'X-Parse-Application-Id': 'HZAMesseJ6CDe1K5dFLfxbGbMYD6aV3lBaEp3Ib1',
+                            'X-Parse-REST-API-Key': 'LZqwu8VIutbaphzVoPW7yf4RxkKQAMbAapwubT5L',
+                            'X-Parse-Session-Token': user ? user.sessionToken : ''
+                        },
+                        method: 'GET',
+                        url: 'https://api.parse.com/1/users/me'
+                    },
+                    d = $q.defer();
+
+                if (user && user.sessionToken.length > 10) {
+                    $http(config)
+                        .success(function(data, status, headers, config) {
+                            $log.info('autoSignon success, data = %o', data);
+                            setSession(data);
+                            d.resolve(data);
+                        })
+                        .error(function(data, status, headers, config) {
+                            d.reject(data.error);
+                        });
+                } else {
+                    d.reject('Invalid Cookie');
+                }
+                return d.promise;
+            };
+
+            res.loggedOn = function () {
+                return res.userObj !== null;
+            };
+
+            res.signoff = function () {
+                clearSession();
+            };
+
+            res.signon = function (aUsr, aPwd) {
+                var params = '?username=' + aUsr + '&password=' + aPwd,
+                    config = {
+                        method: 'GET',
+                        url: ' https://api.parse.com/1/login' + params
+                    };
+
+                return $http(config)
+                    .then(function (response) {
+                        if (typeof response.data === 'object') {
+                            setSession(response.data);
+                            return response.data;
+                        } else {
+                            clearSession();
+                            return $q.reject(response.data);
+                        }
+                    }, function (response) {
+                        // something went wrong
+                        clearSession();
+                        return $q.reject(response.data);
+                    });
+            };
+
+            return res;
         }]);
 }(angular));
 (function (angular) {
@@ -81,7 +253,7 @@
 (function (angular) {
     'use strict';
 
-    angular.module('app').controller('PostsController', ['$scope', '$rootScope', '$location', '$log',
+    angular.module('app').controller('MoviesController', ['$scope', '$rootScope', '$location', '$log',
         function ($scope, $rootScope, $location, $log) {
 
         }]);
@@ -89,7 +261,7 @@
 (function (angular) {
     'use strict';
 
-    angular.module('app').controller('MoviesController', ['$scope', '$rootScope', '$location', '$log',
+    angular.module('app').controller('PostsController', ['$scope', '$rootScope', '$location', '$log',
         function ($scope, $rootScope, $location, $log) {
 
         }]);
