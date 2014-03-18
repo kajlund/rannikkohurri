0c852a15458bd2b3f4cd5901e5a69d477994445c
kajlund <kaj.lund@gmail.com>
2014-03-18 12:25:28 +0200

    Fixed bug in signon method and rearranged whitespace for jslint

--- public/app/svcSession.js
+++ public/app/svcSession.js
@@ -1,75 +1,80 @@
 (function (angular) {
     'use strict';
 
-    angular.module('app').factory('SessionService', function ($rootScope, $http, $cookieStore, $log, $q) {
-        var res = {};
-        res.sessionToken = '';
-        res.userObj = null;
-
-        function setSession(data) {
-            res.userObj = data;
-            res.sessionToken = data.sessionToken;
-            $cookieStore.put('ParseUser', data);
-
-        }
-        function clearSession() {
-            res.userObj = null;
+    angular.module('app').factory('SessionService', ['$rootScope', '$http', '$cookieStore', '$log', '$q',
+        function ($rootScope, $http, $cookieStore, $log, $q) {
+            var res = {};
             res.sessionToken = '';
-            $cookieStore.remove('ParseUser');
-        }
+            res.userObj = null;
 
-        res.autoSignon = function () {
-            var user = $cookieStore.get('ParseUser'),
-                config = {
-                    headers: {
-                        'X-Parse-Application-Id': 'HZAMesseJ6CDe1K5dFLfxbGbMYD6aV3lBaEp3Ib1',
-                        'X-Parse-REST-API-Key': 'LZqwu8VIutbaphzVoPW7yf4RxkKQAMbAapwubT5L',
-                        'X-Parse-Session-Token': user ? user.sessionToken : ''
-                    },
-                    method: 'GET',
-                    url: 'https://api.parse.com/1/users/me'
-                },
-                d = $q.defer();
+            function setSession(data) {
+                res.userObj = data;
+                res.sessionToken = data.sessionToken;
+                $cookieStore.put('ParseUser', data);
 
-            if (user && user.sessionToken.length > 10) {
-                $http(config)
-                    .success(function(data, status, headers, config) {
-                        $log.info('autoSignon success, data = %o', data);
-                        setSession(data);
-                        d.resolve(data);
-                    })
-                    .error(function(data, status, headers, config) {
-                        d.reject(data.error);
-                    });
-            } else {
-                d.reject('Invalid Cookie');
             }
-            return d.promise;
-        };
+            function clearSession() {
+                res.userObj = null;
+                res.sessionToken = '';
+                $cookieStore.remove('ParseUser');
+            }
 
-        res.loggedOn = function () {
-            return res.userObj !== null;
-        };
+            res.autoSignon = function () {
+                var user = $cookieStore.get('ParseUser'),
+                    config = {
+                        headers: {
+                            'X-Parse-Application-Id': 'HZAMesseJ6CDe1K5dFLfxbGbMYD6aV3lBaEp3Ib1',
+                            'X-Parse-REST-API-Key': 'LZqwu8VIutbaphzVoPW7yf4RxkKQAMbAapwubT5L',
+                            'X-Parse-Session-Token': user ? user.sessionToken : ''
+                        },
+                        method: 'GET',
+                        url: 'https://api.parse.com/1/users/me'
+                    },
+                    d = $q.defer();
 
-        res.signon = function (aUsr, aPwd) {
-            var params = '?username=' + aUsr + '&password=' + aPwd,
-                config = {
-                    method: 'GET',
-                    url: ' https://api.parse.com/1/login' + params
-                },
-                d = $q.defer();
+                if (user && user.sessionToken.length > 10) {
+                    $http(config)
+                        .success(function(data, status, headers, config) {
+                            $log.info('autoSignon success, data = %o', data);
+                            setSession(data);
+                            d.resolve(data);
+                        })
+                        .error(function(data, status, headers, config) {
+                            d.reject(data.error);
+                        });
+                } else {
+                    d.reject('Invalid Cookie');
+                }
+                return d.promise;
+            };
 
-            $http(config)
-                .success(function (data, status, headers, config) {
-                    setSession(data);
-                    d.resolve(data);
-                }).error(function (data, status, headers, config) {
-                    clearSession();
-                    d.reject(data.error);
-                });
-            return d.promise();
-        };
+            res.loggedOn = function () {
+                return res.userObj !== null;
+            };
 
-        return res;
-    });
+            res.signon = function (aUsr, aPwd) {
+                var params = '?username=' + aUsr + '&password=' + aPwd,
+                    config = {
+                        method: 'GET',
+                        url: ' https://api.parse.com/1/login' + params
+                    };
+
+                return $http(config)
+                    .then(function (response) {
+                        if (typeof response.data === 'object') {
+                            setSession(response.data);
+                            return response.data;
+                        } else {
+                            clearSession();
+                            return $q.reject(response.data);
+                        }
+                    }, function (response) {
+                        // something went wrong
+                        clearSession();
+                        return $q.reject(response.data);
+                    });
+            };
+
+            return res;
+        }]);
 }(angular));
