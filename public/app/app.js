f542bc7b1c99bd434e70a579d27331c87bd8c1d8
kajlund <kaj.lund@gmail.com>
2014-03-18 21:43:51 +0200

    fixed indentation

--- public/app/app.js
+++ public/app/app.js
@@ -15,33 +15,33 @@
     // Configure Routes
     app.config(['$routeProvider', '$httpProvider',
         function ($routeProvider, $httpProvider) {
-        $httpProvider.defaults.headers.common['X-Parse-Application-Id'] = 'HZAMesseJ6CDe1K5dFLfxbGbMYD6aV3lBaEp3Ib1';
-        $httpProvider.defaults.headers.common['X-Parse-REST-API-Key'] = 'LZqwu8VIutbaphzVoPW7yf4RxkKQAMbAapwubT5L';
+            $httpProvider.defaults.headers.common['X-Parse-Application-Id'] = 'HZAMesseJ6CDe1K5dFLfxbGbMYD6aV3lBaEp3Ib1';
+            $httpProvider.defaults.headers.common['X-Parse-REST-API-Key'] = 'LZqwu8VIutbaphzVoPW7yf4RxkKQAMbAapwubT5L';
 
-        $routeProvider
-            .when('/', {
-                redirectTo: '/home'
-            }).when('/home', {
-                templateUrl: 'app/home.html',
-                controller: 'MainController'
-            }).when('/posts', {
-                templateUrl: 'app/posts/posts.html',
-                controller: 'PostsController'
-            }).when('/links', {
-                templateUrl: 'app/links/links.html',
-                controller: 'LinksController'
-            }).when('/about', {
-                templateUrl: 'app/about.html',
-                controller: 'AboutController'
-            }).when('/books', {
-                templateUrl: 'app/books/books.html',
-                controller: 'BooksController'
-            }).when('/movies', {
-                templateUrl: 'app/movies/movies.html',
-                controller: 'MoviesController'
-            })
-            .otherwise({ redirectTo: '/home' });
-    }]);
+            $routeProvider
+                .when('/', {
+                    redirectTo: '/home'
+                }).when('/home', {
+                    templateUrl: 'app/home.html',
+                    controller: 'MainController'
+                }).when('/posts', {
+                    templateUrl: 'app/posts/posts.html',
+                    controller: 'PostsController'
+                }).when('/links', {
+                    templateUrl: 'app/links/links.html',
+                    controller: 'LinksController'
+                }).when('/about', {
+                    templateUrl: 'app/about.html',
+                    controller: 'AboutController'
+                }).when('/books', {
+                    templateUrl: 'app/books/books.html',
+                    controller: 'BooksController'
+                }).when('/movies', {
+                    templateUrl: 'app/movies/movies.html',
+                    controller: 'MoviesController'
+                })
+                .otherwise({ redirectTo: '/home' });
+        }]);
 
     app.run(['$rootScope', '$location', '$log',
         function($rootScope, $location, $log) {
