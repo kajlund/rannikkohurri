0583773572bbef8deca43d70805c5b7c487f4b4d
kajlund <kaj.lund@gmail.com>
2014-03-19 18:38:51 +0200

    Added functionality for displaying posts

--- public/app/posts/ctrlPosts.js
+++ public/app/posts/ctrlPosts.js
@@ -1,8 +1,13 @@
 (function (angular) {
     'use strict';
 
-    angular.module('app').controller('PostsController', ['$scope', '$rootScope', '$location', '$log',
-        function ($scope, $rootScope, $location, $log) {
-
+    angular.module('app').controller('PostsController', ['$scope', '$log', 'PostDataService',
+        function ($scope, $log, PostDataService) {
+            PostDataService.getPosts()
+                .then(function (data) {
+                    $log.info(data);
+                    $scope.posts = data;
+                    $scope.$apply();
+                });
         }]);
 }(angular));
---
+++ public/app/posts/svcPostData.js
@@ -0,0 +1,28 @@
+(function (angular, Parse) {
+    'use strict';
+
+    Parse.initialize("HZAMesseJ6CDe1K5dFLfxbGbMYD6aV3lBaEp3Ib1",
+        "BxuS4AKpUCoP6Ea6pOn1O0PXlmPu5wYvvlSxLJVE");
+
+    angular.module('app').factory('PostDataService', ['$log', '$q',
+        function ($log, $q) {
+            var res = {},
+                PostModel = Parse.Object.extend({
+                    className: 'Post'
+                });
+
+            res.getPost = function (aId) {
+                var qry = new Parse.Query(PostModel);
+                return qry.get(aId);
+            };
+
+            res.getPosts = function () {
+                var qry = new Parse.Query(PostModel);
+                qry.equalTo("published", true);
+                qry.descending("publishDate");
+                return qry.find();
+            };
+
+            return res;
+        }]);
+}(angular, Parse));
