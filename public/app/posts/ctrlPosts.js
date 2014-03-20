117d3836ef402456c60f7710fe4ddb60db84ab7f
kajlund <kaj.lund@gmail.com>
2014-03-20 22:32:12 +0200

    Added CRUD templates

--- public/app/posts/ctrlPosts.js
+++ public/app/posts/ctrlPosts.js
@@ -12,5 +12,17 @@
                     $rootScope.isBusy = false;
                     $scope.$apply();
                 });
+
+            $scope.onAddClick = function () {
+                $log.info('Adding new post');
+            };
+
+            $scope.onEditClick = function (p) {
+                $log.info('Editing post %o', p);
+            };
+
+            $scope.onDeleteClick = function (p) {
+                $log.info('Deleting post %o', p);
+            };
         }]);
 }(angular));
