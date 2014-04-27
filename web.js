bb58da6b638f802a35b9afa37ebdb6ca39e46dd4
unknown <luka@ARKLA1304-LT-01.aholatransport.com>
2014-04-27 17:26:02 +0300

    removed root path

--- web.js
+++ web.js
@@ -16,10 +16,6 @@ app.get('/dropbox/:slug', function(req, res) {
     });
 });
 
-app.get('/', function(req, res) {
-    res.send('Hello World!');
-});
-
 var port = Number(process.env.PORT || 5000);
 app.listen(port, function() {
     console.log("Listening on " + port);
