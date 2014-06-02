05a792bcefb092f421838213a4620ac2ef52f44e
unknown <luka@ARKLA1304-LT-01.aholatransport.com>
2014-06-02 22:50:53 +0300

    changed default local port to 80

--- web.js
+++ web.js
@@ -16,7 +16,7 @@ app.get('/dropbox/:slug', function(req, res) {
     });
 });
 
-var port = Number(process.env.PORT || 5000);
+var port = Number(process.env.PORT || 80);
 app.listen(port, function() {
     console.log("Listening on " + port);
 });
